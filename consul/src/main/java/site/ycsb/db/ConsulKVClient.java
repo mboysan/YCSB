f01820b837f667acd76a8ed1f967c9c757bd2123
mboysan <dev@mboysan.com>
2024-03-30 14:42:09 +0200

    THESIS-T-101: implement ConsulKVClient

---
+++ consul/src/main/java/site/ycsb/db/ConsulKVClient.java
@@ -0,0 +1,119 @@
+package site.ycsb.db;
+
+import com.ecwid.consul.v1.ConsulClient;
+import com.ecwid.consul.v1.Response;
+import com.ecwid.consul.v1.kv.model.GetValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import site.ycsb.ByteIterator;
+import site.ycsb.DB;
+import site.ycsb.Status;
+import site.ycsb.StringByteIterator;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
+import java.util.Set;
+import java.util.Vector;
+
+/**
+ * <a href="https://developer.hashicorp.com/consul/">consul</a> binding for YCSB.
+ */
+public class ConsulKVClient extends DB {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(ConsulKVClient.class);
+
+  private ConsulClient kvStoreClient;
+
+  @Override
+  public void init() {
+    Properties properties = getProperties();
+    LOGGER.info("properties={}", properties);
+    String clusterMembers = properties.getProperty("cluster.members");
+    List<ConsulDestination> destinations = convertPropsToDestinationsList(clusterMembers);
+    if (destinations.size() > 1) {
+      throw new IllegalArgumentException("Multiple destinations are not supported");
+    }
+    ConsulDestination destination = destinations.get(0);
+    this.kvStoreClient = new ConsulClient(destination.getAddress(), destination.getPort());
+  }
+
+  @Override
+  public Status read(String table, String key, Set<String> fields, Map<String, ByteIterator> result) {
+    try {
+      Response<GetValue> keyValueResponse = kvStoreClient.getKVValue(key);
+      GetValue getValue = keyValueResponse.getValue();
+      String value = null;
+      if (getValue != null) {
+        value = getValue.getDecodedValue();
+      }
+      result.put(key, new StringByteIterator(value));
+      return Status.OK;
+    } catch (Exception e) {
+      LOGGER.error("[read] error at table={}, key={}, err={}", table, key, e.getMessage(), e);
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status scan(String table, String startkey, int recordcount, Set<String> fields,
+                     Vector<HashMap<String, ByteIterator>> result) {
+    return Status.NOT_IMPLEMENTED;
+  }
+
+  @Override
+  public Status update(String table, String key, Map<String, ByteIterator> values) {
+    try {
+      String valuesStr = values.toString();
+      kvStoreClient.setKVValue(key, valuesStr);
+      return Status.OK;
+    } catch (Exception e) {
+      LOGGER.error("[update] error at table={}, key={}, err={}", table, key, e.getMessage(), e);
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status insert(String table, String key, Map<String, ByteIterator> values) {
+    try {
+      String valuesStr = values.toString();
+      kvStoreClient.setKVValue(key, valuesStr);
+      return Status.OK;
+    } catch (Exception e) {
+      LOGGER.error("[insert] error at table={}, key={}, err={}", table, key, e.getMessage(), e);
+      return Status.ERROR;
+    }
+  }
+
+  @Override
+  public Status delete(String table, String key) {
+    try {
+      kvStoreClient.deleteKVValue(key);
+      return Status.OK;
+    } catch (Exception e) {
+      LOGGER.error("[delete] error at table={}, key={}, err={}", table, key, e.getMessage(), e);
+      return Status.ERROR;
+    }
+  }
+
+  private static List<ConsulDestination> convertPropsToDestinationsList(String destinationProps) {
+    Objects.requireNonNull(destinationProps);
+    List<ConsulDestination> destinations = new ArrayList<>();
+    destinationProps = destinationProps.replaceAll("\\s+", "");    // remove whitespace
+    String[] dests = destinationProps.split(",");
+    for (String dest : dests) {
+      String[] idIp = dest.split("=");
+      String id = idIp[0];
+      String addrPort = idIp[1];
+      String[] addrPortArray = addrPort.split(":");
+      String address = addrPortArray[0];
+      int port = Integer.parseInt(addrPortArray[1]);
+      destinations.add(new ConsulDestination(id, address, port));
+    }
+    return destinations;
+  }
+
+}
---
+++ consul/src/test/java/site/ycsb/db/ConsulKVClientIntegrationTest.java
@@ -0,0 +1,95 @@
+package site.ycsb.db;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import site.ycsb.ByteIterator;
+import site.ycsb.Status;
+import site.ycsb.StringByteIterator;
+import site.ycsb.measurements.Measurements;
+import site.ycsb.workloads.CoreWorkload;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static junit.framework.Assert.assertNull;
+import static junit.framework.TestCase.assertEquals;
+
+public class ConsulKVClientIntegrationTest {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ConsulKVClient.class);
+
+  private static final String TABLE = "sometable";
+
+  private ConsulKVClient client;
+
+  @BeforeClass
+  public static void beforeClass() throws Exception {
+    LOGGER.info("simplelogger test");
+  }
+
+  @Before
+  public void setUp() throws Exception {
+    client = new ConsulKVClient();
+
+    Properties properties = new Properties();
+    properties.put("cluster.members", "agent0=localhost:8500");
+
+    Measurements.setProperties(properties);
+    final CoreWorkload workload = new CoreWorkload();
+    workload.init(properties);
+
+    client.setProperties(properties);
+    client.init();
+  }
+
+  @Test
+  public void testConsulClient() {
+    final String testKey = "someKey";
+
+    // insert
+    Map<String, String> expectedValues = new HashMap<>();
+    String field1 = "field_1";
+    String value1 = "value_1";
+    expectedValues.put(field1, value1);
+    Map<String, ByteIterator> result = StringByteIterator.getByteIteratorMap(expectedValues);
+    client.insert(TABLE, testKey, result);
+
+    // read
+    result.clear();
+    Status status = client.read(TABLE, testKey, null, result);
+    assertEquals(Status.OK, status);
+    assertEquals(1, result.size());
+    assertEquals(expectedValues.toString(), result.get(testKey).toString());
+
+    // update(the same field)
+    expectedValues.clear();
+    result.clear();
+    String newVal = "value_new";
+    expectedValues.put(field1, newVal);
+    result = StringByteIterator.getByteIteratorMap(expectedValues);
+    client.update(TABLE, testKey, result);
+    assertEquals(1, result.size());
+
+    // Verify result
+    result.clear();
+    status = client.read(TABLE, testKey, null, result);
+    assertEquals(Status.OK, status);
+    // here we only have one field: field_1
+    assertEquals(1, result.size());
+    assertEquals(expectedValues.toString(), result.get(testKey).toString());
+
+    // delete
+    status = client.delete(TABLE, testKey);
+    assertEquals(Status.OK, status);
+
+    // Verify result
+    result.clear();
+    status = client.read(TABLE, testKey, null, result);
+    // NoNode return ERROR
+    assertEquals(Status.OK, status);
+    assertNull(result.get(testKey).toString());
+  }
+}
